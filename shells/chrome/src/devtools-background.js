6fa3316cffe54bc4ac967829bdd48b574ca65ff6
炒饭 <chaofan@2dfire.com>
2017-01-10 11:35:51 +0800

    结构搭建完成

--- shells/chrome/src/devtools-background.js
+++ shells/chrome/src/devtools-background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 chrome.devtools.panels.create(
-    'auto cat', 'icon.png', 'panel.html',
+    'auto cat', 'icon.png', 'devtools.html',
     function (panel) {
       // 面板创建时调用的代码
     }
---
+++ shells/dev/webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+const alias = require('../alias');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].build.js',
+
+    libraryTarget: 'umd',
+    pathinfo: true,
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/'
+  },
+  externals: {
+    'jquery': 'jQuery'
+  },
+  resolve: {
+    alias
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          'presets': ['es2015', 'stage-3'],
+          'comments': false
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: 'less-loader'
+      },
+
+      { test: /\.css$/, loaders: ['style', 'css'] },
+      // => 'style' and 'css' loader is used for '.css' files
+      {
+        test: /\.(png|woff2)$/,
+        loader: 'url-loader?limit=0'
+      }
+    ]
+  },
+
+  target: 'web',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ],
+
+  devtool: 'source-map'
+};
